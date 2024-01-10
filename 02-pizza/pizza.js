70a16f69db4fe971d0f8cbb93c5590ee9b9bd854
Amoy N <117183421+norusAm@users.noreply.github.com>
2024-01-10 16:15:46 -0500

    Update pizza.js

--- 02-pizza/pizza.js
+++ 02-pizza/pizza.js
@@ -1,24 +1,46 @@
 // Create a pizza object that has four properties and three methods
 
-let pizza = {};
+// let pizza = {};
 
-pizza.size = 'large';
-pizza.toppings = ['spinach', 'onions', 'peppers', 'bell peppers'];
-pizza.crust = 'stuffed';
-pizza.sauce = 'heavy';
+// pizza.size = 'large';
+// pizza.toppings = ['spinach', 'onions', 'peppers', 'bell peppers'];
+// pizza.crust = 'stuffed';
+// pizza.sauce = 'heavy';
 
-pizza.estimateDeliveryTime = function() {
-    console.log('Calculating...');
+// pizza.estimateDeliveryTime = function() {
+//     console.log('Calculating...');
+// }
+
+// pizza.estimateDeliveryTime = function() {
+//     console.log('Calculating...');
+// }
+
+// pizza.burnMouth = function() {
+//     console.log('Yikes....');
+// }
+
+// pizza.frisbee = function() {
+//     console.log('Roll baby');
+// }
+
+
+function Pizza(size, toppings, crust, sauce) {
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.sauce = sauce;
+
+    this.estimateDeliveryTime = function() {
+       console.log('Calculating...');
+    }
+    
+    this.burnMouth = function() {
+        console.log('Yikes....');
+    }
+
+    this.frisbee = function() {
+        console.log('Roll baby');
+    }
+
 }
 
-pizza.estimateDeliveryTime = function() {
-    console.log('Calculating...');
-}
-
-pizza.burnMouth = function() {
-    console.log('Yikes....');
-}
-
-pizza.frisbee = function() {
-    console.log('Roll baby');
-}
