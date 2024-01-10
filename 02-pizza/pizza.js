16c4ad2a6b21a4d867f187bfbd23698c389cd3ba
Amoy N <117183421+norusAm@users.noreply.github.com>
2024-01-10 15:58:04 -0500

    Create pizza.js

---
+++ 02-pizza/pizza.js
@@ -0,0 +1,24 @@
+// Create a pizza object that has four properties and three methods
+
+let pizza = {};
+
+pizza.size = 'large';
+pizza.toppings = ['spinach', 'onions', 'peppers', 'bell peppers'];
+pizza.crust = 'stuffed';
+pizza.sauce = 'heavy';
+
+pizza.estimateDeliveryTime = function() {
+    console.log('Calculating...');
+}
+
+pizza.estimateDeliveryTime = function() {
+    console.log('Calculating...');
+}
+
+pizza.burnMouth = function() {
+    console.log('Yikes....');
+}
+
+pizza.frisbee = function() {
+    console.log('Rolll baby');
+}
