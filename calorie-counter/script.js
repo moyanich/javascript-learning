c9261aee610ff1db430ffcc8c23ccd387a063e2e
Amoy N <ganymedenick@gmail.com>
2024-01-30 15:52:26 -0500

    Update script.js

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -7,6 +7,28 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-    const strArray = str.split('');
-    const cleanStrArray = [];
+    // const strArray = str.split('');
+    // const cleanStrArray = [];
+    // const patternsArray = ["+", "-", " "];
+
+    // for (let i = 0; i < strArray.length; i++) {
+    //     if (!["+", "-", " "].includes(strArray[i])) {
+    //         cleanStrArray.push(strArray[i]);
+    //     }
+    // }
+
+    // console.table(cleanStrArray);
+
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");
+
+
+    
 }
+
+function isInvalidInput(str){
+    const regex = /e/i;
+}
+
+
+cleanInputString('hello');
