e52f4e4189a5f0a09090fde62e2ff7ffbdb70ecc
Amoy N <ganymedenick@gmail.com>
2024-02-01 10:41:00 -0500

    Update script.js

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -21,14 +21,22 @@ function cleanInputString(str) {
 
     const regex = /[+-\s]/g;
     return str.replace(regex, "");
-
-
     
 }
 
 function isInvalidInput(str){
-    const regex = /e/i;
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetId = '#' + entryDropdown.value;
+    const targetInputContainer = document.querySelector(`${targetId} .input-container`);
+
+  
 }
 
 
 cleanInputString('hello');
+
+console.log( addEntry());
