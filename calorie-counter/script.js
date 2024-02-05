3b996d7d9e8c928c92bcbd2bb88ed670e2d04953
Amoy N <ganymedenick@gmail.com>
2024-02-05 15:29:05 -0500

    Update script.js

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -68,10 +68,15 @@ function calculateCalories(e){
     const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
     const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
     const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 
-  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
 
-  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 }
 
 
