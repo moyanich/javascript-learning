881f53b5367bee4059763c6fbd78521b0a525381
Amoy N <117183421+norusAm@users.noreply.github.com>
2024-02-12 19:09:38 -0500

    Update script.js

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -76,6 +76,7 @@ function calculateCalories(e){
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 
 }
 
