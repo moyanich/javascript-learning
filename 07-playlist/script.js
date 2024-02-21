1c9b86801ba22a2c283e9d751b6d5580bb42f2a8
Amoy N <117183421+norusAm@users.noreply.github.com>
2024-02-21 15:41:30 -0500

    Update script.js

--- 07-playlist/script.js
+++ 07-playlist/script.js
@@ -111,7 +111,13 @@ const renderSongs = (array) => {
 };
 
 userData?.songs.sort((a, b) => {
-
+	if (a.title < b.title) {
+		return -1;
+	}
+	if (a.title > b.title) {
+		return 1;
+	}
+	return 0;
 });
 
 renderSongs(userData?.songs);
